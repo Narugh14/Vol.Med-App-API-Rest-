0fd24c84c179aab132d5cf6e7ca0ee760b917dfc
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2025-07-01 08:52:52 -0600

    Agregamos al paginado el Id f/Paciente

--- src/main/java/com/Narugh14/API/paciente/DatosListPaciente.java
+++ src/main/java/com/Narugh14/API/paciente/DatosListPaciente.java
@@ -1,12 +1,14 @@
 package com.Narugh14.API.paciente;
 
 public record DatosListPaciente(
+        Long id,
         String nombre,
         String email,
         String documento
 ) {
     public DatosListPaciente(Paciente paciente) {
-        this(paciente.getNombre(),
+        this(   paciente.getId(),
+                paciente.getNombre(),
                 paciente.getEmail(),
                 paciente.getDocumento());
     }
