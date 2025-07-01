98364c9b0d1761bf94d8edcb59ba9b77fa1ba731
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2025-07-01 09:16:22 -0600

    Se agrega @NotNull Id Medico y Paciente

--- src/main/java/com/Narugh14/API/medico/DatosActualizarMedico.java
+++ src/main/java/com/Narugh14/API/medico/DatosActualizarMedico.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record DatosActualizarMedico(
-        Long id,
+        @NotNull  Long id,
         String nombre,
         String telefono,
         DatosDireccion direccion
--- src/main/java/com/Narugh14/API/paciente/DatosActualizarPaciente.java
+++ src/main/java/com/Narugh14/API/paciente/DatosActualizarPaciente.java
@@ -1,9 +1,10 @@
 package com.Narugh14.API.paciente;
 
 import com.Narugh14.API.direccion.DatosDireccion;
+import jakarta.validation.constraints.NotNull;
 
 public record DatosActualizarPaciente(
-        Long id,
+        @NotNull Long id,
         String nombre,
         String telefono,
         DatosDireccion direccion
