737b874e8dc19d0c73f84d2bfa8a588e17b295cd
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2025-07-06 11:16:47 -0600

    Se agrega metodo para Error-400
    Este metodo es para expresar que no se puede crear un nuevo usuario si no tiene todos los campones requeridos, tambien tiene su propio DTO para mostrar que campo y que es lo que pasa con ese campo

--- src/main/java/com/Narugh14/API/infra/GestorDeErrores.java
+++ src/main/java/com/Narugh14/API/infra/GestorDeErrores.java
@@ -2,8 +2,12 @@ package com.Narugh14.API.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GestorDeErrores {
@@ -12,4 +16,19 @@ public class GestorDeErrores {
     public ResponseEntity gestionarError404(){
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity gestionarError400(MethodArgumentNotValidException ex){
+        List<FieldError> errores = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValid::new).toList());
+    }
+
+    public record DatosErrorValid(
+            String campo, String mensaje){
+        public DatosErrorValid(FieldError ex){
+            this(ex.getField(),
+                    ex.getDefaultMessage());
+        }
+    }
+
 }
